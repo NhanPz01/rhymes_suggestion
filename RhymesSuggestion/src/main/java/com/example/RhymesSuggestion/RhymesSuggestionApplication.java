3132dee812661beac7effbec6364b91d394e956f
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-09-04 23:06:44 +0700

    create base

---
+++ RhymesSuggestion/src/main/java/com/example/RhymesSuggestion/RhymesSuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.example.RhymesSuggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhymesSuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhymesSuggestionApplication.class, args);
+	}
+
+}
---
+++ RhymesSuggestion/src/test/java/com/example/RhymesSuggestion/RhymesSuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RhymesSuggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhymesSuggestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
